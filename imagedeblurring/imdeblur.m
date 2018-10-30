8cd58db0480d69b1e25cfaad044c7249a4dea2d8
Michael Sosa <mcs348@cornell.edu>
2018-10-30 02:17:38 -0400

    fix issue, now 1 dimensional so can use cg

--- imagedeblurring/imdeblur.m
+++ imagedeblurring/imdeblur.m
@@ -1,6 +1,10 @@
 % load scary skeletons
 im = imread('spook.jpg');
-% image(im);
+% colormap(gray(256));
+% by converting to greyscale no longer need 3 dim
+% https://www.mathworks.com/help/matlab/ref/rgb2gray.html
+im = .2989*im(:,:,1) + .5870*im(:,:,2) + .1140*im(:,:,3);
+im = im2double(im);
 [m, n, ~] = size(im);
 npts = m*n;
 
@@ -9,18 +13,14 @@ sig=1.5;
 wid=3;
 % Add noise at 1% level
 scale=0.01;
-tset = zeros(1,m-wid);
 a= [exp(-((0:wid-1).^2)/(2*sig^2)), zeros(1,m-wid)];
 g= toeplitz(a);
 g= sparse(g);
 g= (1/(2*pi*sig^2))*kron(g,g);
 
 % generate blurred image
-imblur = im;
-for i=1:3
-    imr = double(reshape(im(:,:,i), npts, 1));
-    imb = g * temp + scale * randn(npts, 1);
-    imblur(:,:,i) = reshape(imb, m,n);
-end
+imr = reshape(im, npts, 1);
+imb = g * imr + scale * randn(npts, 1);
+imblur = reshape(imb, m,n);
 
-vblur = imshow(imblur);
+% imshow(imblur);
